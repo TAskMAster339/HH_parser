0c933c96315dc09a90f6c3375f38682c8e81b139
Dima <dimagr3214@gmail.com>
2024-07-04 21:57:46 +0300

    Add docker, docker-compose

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from os import environ
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/parser"
+DATABASE_URL = f"postgresql://postgres:admin@{environ.get("HOSTNAME")}:5432/parser"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
